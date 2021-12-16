be448a28620869cf329611f13d2a5794413ae691
Christoph Reiter <reiter.christoph@gmail.com>
2021-12-16 15:46:31 +0100

    Add some more types

--- src/Student/StudentData.php
+++ src/Student/StudentData.php
@@ -6,9 +6,28 @@ namespace Dbp\CampusonlineApi\Student;
 
 class StudentData
 {
+    /**
+     * @var string
+     */
     public $firstName;
+
+    /**
+     * @var string
+     */
     public $lastName;
+
+    /**
+     * @var string
+     */
     public $identId;
+
+    /**
+     * @var string
+     */
     public $identIdObfuscated;
+
+    /**
+     * @var string
+     */
     public $personId;
 }
