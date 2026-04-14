7babccb638afcebe5b66520ef92acf3fe4dbefef
Tobias Gross-Vogt <tobias.gross-vogt@tugraz.at>
2026-04-14 09:39:16 +0200

    PublicRestApi/UserApi: add query parameter constants

--- src/PublicRestApi/Accounts/UserApi.php
+++ src/PublicRestApi/Accounts/UserApi.php
@@ -13,6 +13,8 @@ class UserApi extends AbstractApi
     public const PERSON_UID_QUERY_PARAMETER_NAME = 'person_uid';
     public const ACCOUNT_TYPE_KEY_QUERY_PARAMETER_NAME = 'account_type_key';
     public const ACCOUNT_STATUS_KEY_QUERY_PARAMETER_NAME = 'account_status_key';
+    public const USERNAME_QUERY_PARAMETER_NAME = 'username';
+    public const EMAIL_QUERY_PARAMETER_NAME = 'email';
 
     private const API_PATH = Common::API_PATH.'/users/search';
 
